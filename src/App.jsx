688b447083a4e56fc6e47bec5690eecce984faa8
Walter Reyes <desarrolladorweb@grupofajardo.com.ec>
2024-08-13 22:07:17 -0500

    add: agregar custom hook para separar logica en useCart y exportando todas las funciones y hooks propios de react

--- src/App.jsx
+++ src/App.jsx
@@ -1,77 +1,20 @@
-import { useEffect, useState } from 'react'
+
 import './App.css'
 import { Header } from './components/Header'
 import { Producto } from './components/Producto'
-import productos from './data/bd_productos'
+import { useCart } from './hook/useCart'
 
 function App() {
 
-  const initialCart = JSON.parse(localStorage.getItem('cart')) || []
-
-  const [data, setData] = useState([])
-  const [cart, setCart] = useState(initialCart)
-
-  const MAX_PRODUCTS_CART = 3
-
-  useEffect(() => {
-    setData(productos)
-  }, [])
-
-  useEffect(() => {
-    localStorage.setItem('cart', JSON.stringify(cart))
-  }, [cart])
-
-  const addProductToCart = (producto) => {
-    const item = cart.findIndex(prod => prod.id === producto.id)
-    if (item >= 0) {
-      if (cart[item].cantidad < MAX_PRODUCTS_CART) {
-        console.log('El producto ya está en el carrito')
-        const updatedCart = [...cart]
-        updatedCart[item].cantidad++
-        setCart(updatedCart)
-      }
-    } else {
-      console.log('Producto añadido al carrito')
-      setCart([...cart, { ...producto, cantidad: 1 }])
-    }
-  }
-
-  const removeProductFromCart = (id) => {
-    console.log('Producto eliminado del carrito:' + id)
-    setCart(prevCar => prevCar.filter(prod => prod.id !== id))
-  }
-
-  const increaseQuantity = (id) => {
-    //opcion 1
-    /* setCart(prevCar => prevCar.map(prod => {
-      if (prod.id === id && prod.cantidad < MAX_PRODUCTS_CART) {
-        return { ...prod, cantidad: prod.cantidad + 1 }
-      } else {
-        return prod
-      }
-    })) */
-
-    //opcion 2
-    const index = cart.findIndex(prod => prod.id === id)
-    if (index >= 0 && cart[index].cantidad < MAX_PRODUCTS_CART) {
-      const updatedCart = [...cart]
-      updatedCart[index].cantidad++
-      setCart(updatedCart)
-    }
-  }
-
-  const decreaseQuantity = (id) => {
-    const index = cart.findIndex(prod => prod.id === id)
-    if (index >= 0 && cart[index].cantidad > 1) {
-      const updatedCart = [...cart]
-      updatedCart[index].cantidad--
-      setCart(updatedCart)
-    }
-  }
-
-  const clearCart = () => {
-    setCart([])
-  }
+  const {
+    data,
+    cart,
+    addProductToCart,
+    removeProductFromCart,
+    increaseQuantity,
+    decreaseQuantity,
+    clearCart
+  } = useCart()
 
   return (
     <>
---
+++ src/hook/useCart.js
@@ -0,0 +1,83 @@
+import { useEffect, useState } from 'react'
+import productos from '../data/bd_productos'
+
+export const useCart = () => {
+
+    const initialCart = JSON.parse(localStorage.getItem('cart')) || []
+
+    const [data, setData] = useState([])
+    const [cart, setCart] = useState(initialCart)
+
+    const MAX_PRODUCTS_CART = 3
+
+    useEffect(() => {
+        setData(productos)
+    }, [])
+
+    useEffect(() => {
+        localStorage.setItem('cart', JSON.stringify(cart))
+    }, [cart])
+
+    const addProductToCart = (producto) => {
+        const item = cart.findIndex(prod => prod.id === producto.id)
+        if (item >= 0) {
+            if (cart[item].cantidad < MAX_PRODUCTS_CART) {
+                console.log('El producto ya está en el carrito')
+                const updatedCart = [...cart]
+                updatedCart[item].cantidad++
+                setCart(updatedCart)
+            }
+        } else {
+            console.log('Producto añadido al carrito')
+            setCart([...cart, { ...producto, cantidad: 1 }])
+        }
+    }
+
+    const removeProductFromCart = (id) => {
+        console.log('Producto eliminado del carrito:' + id)
+        setCart(prevCar => prevCar.filter(prod => prod.id !== id))
+    }
+
+    const increaseQuantity = (id) => {
+        //opcion 1
+        /* setCart(prevCar => prevCar.map(prod => {
+          if (prod.id === id && prod.cantidad < MAX_PRODUCTS_CART) {
+            return { ...prod, cantidad: prod.cantidad + 1 }
+          } else {
+            return prod
+          }
+        })) */
+
+        //opcion 2
+        const index = cart.findIndex(prod => prod.id === id)
+        if (index >= 0 && cart[index].cantidad < MAX_PRODUCTS_CART) {
+            const updatedCart = [...cart]
+            updatedCart[index].cantidad++
+            setCart(updatedCart)
+        }
+    }
+
+    const decreaseQuantity = (id) => {
+        const index = cart.findIndex(prod => prod.id === id)
+        if (index >= 0 && cart[index].cantidad > 1) {
+            const updatedCart = [...cart]
+            updatedCart[index].cantidad--
+            setCart(updatedCart)
+        }
+    }
+
+    const clearCart = () => {
+        setCart([])
+    }
+
+    return {
+        data,
+        cart,
+        addProductToCart,
+        removeProductFromCart,
+        increaseQuantity,
+        decreaseQuantity,
+        clearCart
+    }
+
+}
